052fb6f4256b38bfce8a13830224d2b6fb3644c1
Danilo Oliveira <daniloreisn@gmail.com>
2023-11-28 17:53:18 -0400

    novo teste

---
+++ app.js
@@ -0,0 +1,17 @@
+
+
+function exibirTexto(tag,texto){
+    let campo  = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+exibirTexto('h1','Jogo do número secreto');
+exibirTexto('p','Escolha um número entre 1 e 10');
+
+function verificarChute(){
+    
+}
+
+function gerarNumeroAleatorio(){
+    return parseInt(Math.random() * 10 + 1);
+}
